ebd47e5d950c7eb1ba95bc697b2d813bb9f03979
Pablo Menz <p.menz.r@gmail.com>
2020-03-19 19:34:18 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+
+class School 
+  
+  attr_accessor :roster, :name
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster[grade]==nil
+    @roster[grade] = []
+    end
+    if  name.instance_of? Array
+      
+    @roster[grade] += name
+    end
+    @roster[grade]<< name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort 
+    new_roster = {}
+    @roster.each do |key, element| 
+      new_roster[key] = element.sort!
+      
+    end
+    @roster = new_roster
+    
+end
+end
+
+  
+  
